600016d76bbbb503c4601bcfdf1c99f31b63c155
Doman <domanski@basement.pl>
2021-09-23 15:36:50 +0200

    comments

--- server/request_handler/message_handlers/Spell.py
+++ server/request_handler/message_handlers/Spell.py
@@ -7,5 +7,14 @@ class SpellHandler(MessageHandler):
     response_message = None
 
     def handle_message(self):
+        """
+        Publish event of spell being used
+        """
         spell_data = self.message.data
         self.session.set_spell(spell_data)
+
+    def build_response(self):
+        """
+        Responds with CombatData
+        """
+        pass
